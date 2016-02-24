4b3feb9290e4717ba059783b2a1acffabb5e3edc
Shaun Enslin <shaun@rapidtrade.biz>
2016-02-24 21:34:20 +0200

    changes

--- file/file.go
+++ file/file.go
@@ -10,12 +10,16 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"sync"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/storage/v1"
 )
+
+var wg sync.WaitGroup
 
 // ZipIT This will ZIP a file and delete the source file after zip
 func ZipIT(source string, target string) error {
@@ -70,6 +74,38 @@ func ZipIT(source string, target string) error {
 	})
 	err = os.Remove(source)
 	return err
+}
+
+// UnZipIT unzips a file to the current folder
+// and returns the file names in a slice
+func UnZipIT(fileName string) ([]string, error) {
+	// Open a zip archive for reading.
+	r, err := zip.OpenReader(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	// Iterate through the files in the archive,
+	// printing some of their contents.
+	var results []string
+	for _, f := range r.File {
+		results = append(results, f.Name)
+		/*
+		   	fmt.Printf("Contents of %s:\n", f.Name)
+		       rc, err := f.Open()
+		       if err != nil {
+		           return "", err
+		       }
+		       _, err = io.CopyN(os.Stdout, rc, 68)
+		       if err != nil {
+		           return "", err
+		       }
+		       rc.Close()
+		       fmt.Println()
+		*/
+	}
+	return results, err
 }
 
 // LoadProperties Loads JSON properties into a MAP,
@@ -141,37 +177,37 @@ func SendGS(fileName string, bucketName string) error {
 	return os.Remove(fileName)
 }
 
-// DownloadGS downloads all files in a Google Storage bucket
-func DownloadGS(bucketName string, folder string) error {
+// DownloadGS downloads all files in a Google Storage bucket and returns a list of files downloaded
+func DownloadGS(bucketName string, folder string) ([]string, error) {
 	client, err := google.DefaultClient(context.Background(), storage.DevstorageFullControlScope)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	service, err := storage.New(client)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	// List all objects in a bucket using pagination
+	var results []string
 	pageToken := ""
 	for {
 		call := service.Objects.List(bucketName)
 		if pageToken != "" {
 			call = call.PageToken(pageToken)
 		}
-		_ = "breakpoint"
 		res, err := call.Do()
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		for _, object := range res.Items {
 			if res, err := service.Objects.Get(bucketName, string(object.Name)).Do(); err == nil {
-				err := DownloadFile(folder, res.Name, res.MediaLink)
-				if err != nil {
-					return err
-				}
+				DownloadFile(folder, res.Name, res.MediaLink)
+
+				fmt.Println("%s", res.MediaLink)
+				results = append(results, res.Name)
 			} else {
-				return err
+				return nil, err
 			}
 		}
 		if pageToken = res.NextPageToken; pageToken == "" {
@@ -179,18 +215,90 @@ func DownloadGS(bucketName string, folder string) error {
 		}
 	}
 
-	return nil
+	return results, nil
 }
 
 // DownloadFile Downloads a file from a URL
-func DownloadFile(folder string, filename string, url string) error {
-	out, err := os.Create(folder + "/" + filename)
-	defer out.Close()
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
-	_, err = io.Copy(out, resp.Body)
+func DownloadFile(folder string, filename string, url string) (string, error) {
+	//ext := filepath.Ext(url)
+
+	file_path := folder + "/" + filename
+
+	os.Mkdir(folder, 0666)
+
+	file, err := os.Create(file_path)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	defer file.Close()
+
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	file_content, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	// returns file size and err
+	_, err = file.Write(file_content)
+
+	if err != nil {
+		return "", err
+	}
+
+	return folder, nil
+}
+
+// DownloadFile Downloads a file from a URL
+func DownloadFileo(folder string, filename string, url string) {
+	/*
+		out, err := os.Create(folder + "/" + filename)
+		defer out.Close()
+		resp, err := http.Get(url)
+		defer resp.Body.Close()
+		//_, err = io.Copy(out, resp.Body)
+		file_content, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		return nil
+	*/
+	_ = "breakpoint"
+	res, _ := http.Head(url) // 187 MB file of random numbers per line
+	maps := res.Header
+	length, _ := strconv.Atoi(maps["Content-Length"][0]) // Get the content length from the header request
+	limit := 3                                           // 10 Go-routines for the process so each downloads 18.7MB
+	lenSub := length / limit                             // Bytes for each Go-routine
+	diff := length % limit                               // Get the remaining for the last request
+	body := make([]string, 11)                           // Make up a temporary array to hold the data to be written to the file
+	for i := 0; i < limit; i++ {
+		wg.Add(1)
+
+		min := lenSub * i       // Min range
+		max := lenSub * (i + 1) // Max range
+
+		if i == limit-1 {
+			max += diff // Add the remaining bytes in the last request
+		}
+
+		go func(min int, max int, i int) {
+			client := &http.Client{}
+			req, _ := http.NewRequest("GET", url, nil)
+			rangeHeader := "bytes=" + strconv.Itoa(min) + "-" + strconv.Itoa(max-1) // Add the data for the Range header of the form "bytes=0-100"
+			req.Header.Add("Range", rangeHeader)
+			resp, _ := client.Do(req)
+			defer resp.Body.Close()
+			reader, _ := ioutil.ReadAll(resp.Body)
+			body[i] = string(reader)
+			ioutil.WriteFile(strconv.Itoa(i), []byte(string(body[i])), 0x777) // Write to the file i as a byte array
+			wg.Done()
+			//          ioutil.WriteFile("new_oct.png", []byte(string(body)), 0x777)
+		}(min, max, i)
+	}
+	wg.Wait()
 }
